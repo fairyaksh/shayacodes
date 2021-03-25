bcda3f7fa84170e75a072cc05dde696c34e348b9
Akshaya <akshayasatheesh@hotmail.co.uk>
2021-03-25 16:14:21 +0000

    Added css to config, checking if it works

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
-module.exports = {
-  dir: {
-    input: "src",
-  },
+module.exports = (eleventyConfig) => {
+  eleventyConfig.addPassthroughCopy("style.css");
+  eleventyConfig.addPassthroughCopy("src");
+  // dir: {
+  //   input: "src",
+  // },
 };

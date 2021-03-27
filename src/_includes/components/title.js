1bd2731f260e8562b4b75aa5506a2de70f3b14aa
Akshaya <akshayasatheesh@hotmail.co.uk>
2021-03-27 16:28:48 +0000

    Commented out as don't need it now

--- src/_includes/components/title.js
+++ src/_includes/components/title.js
@@ -1,17 +1,17 @@
-const React = require("react");
-const html = require("htm").bind(React.createElement);
+// const React = require("react");
+// const html = require("htm").bind(React.createElement);
 
-function Title({ as: Tag = "h1", style, ...rest }) {
-  return html`
-    <${Tag}
-      style="${{
-        fontWeight: 900,
-        color: "hsl(180, 10%, 25%)",
-        ...style,
-      }}"
-      ...${rest}
-    />
-  `;
-}
+// function Title({ as: Tag = "h1", style, ...rest }) {
+//   return html`
+//     <${Tag}
+//       style="${{
+//         fontWeight: 900,
+//         color: "hsl(180, 10%, 25%)",
+//         ...style,
+//       }}"
+//       ...${rest}
+//     />
+//   `;
+// }
 
-module.exports = Title;
+// module.exports = Title;

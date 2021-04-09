b7c934036782867b938c3096b3d00afb2bc4c042
Akshaya <akshayasatheesh@hotmail.co.uk>
2021-04-09 14:00:49 +0100

    Fixed bug - missing input closing tag

--- src/contact.11ty.js
+++ src/contact.11ty.js
@@ -8,24 +8,22 @@ exports.data = {
 exports.render = (data) => {
   return html`
     <h1 class="contact-title">Contact me</h1>
-    <div class="slide-text>
+    <div class="slide-text">
       <h2 class="contact-subtitle">
         Hello. Come say hi. 
       </h2>
     </div>
     <section class="contact-wrapper">
-      <form id="contact-form>
-        <div class="wrap-input">
-          <div class="wrap-input_text">
-            <input placeholder="Your name" type="text" name="name">
-          </div>
-          <div class="wrap-input_text">
-            <input placeholder="Your email" type="email" name="email">
-          </div>
-          <div class="wrap-input_text">
-            <textarea placeholder="Drop a message" type="text" name="email">
-          </div>
-        </div>
+      <form id="contact-form">
+      <div class="wrap-input">
+      <div class="wrap-input_text">
+        <input placeholder="Your name" type="text" name="name"/>
+      </div>
+      <div class="wrap-input_text">
+        <input placeholder="Your email" type="email" name="email"/>
+      </div>
+      </div>
+        <textarea placeholder="Drop a message" type="text" form="contact-form"/>
       </form>
     </section>
   `;

66d4a9676cf331d495b02ade01ad922eeb2537c0
Akshaya <akshayasatheesh@hotmail.co.uk>
2021-04-09 14:07:13 +0100

    Content changes

--- src/contact.11ty.js
+++ src/contact.11ty.js
@@ -7,23 +7,28 @@ exports.data = {
 
 exports.render = (data) => {
   return html`
-    <h1 class="contact-title">Contact me</h1>
+  <h1 class="contact-title">Contact me</h1>
     <div class="slide-text">
       <h2 class="contact-subtitle">
-        Hello. Come say hi. 
+        <span class="emoji">👋</span> Hello. Come say hi. 
       </h2>
     </div>
     <section class="contact-wrapper">
       <form id="contact-form">
-      <div class="wrap-input">
-      <div class="wrap-input_text">
-        <input placeholder="Your name" type="text" name="name"/>
-      </div>
-      <div class="wrap-input_text">
-        <input placeholder="Your email" type="email" name="email"/>
-      </div>
-      </div>
-        <textarea placeholder="Drop a message" type="text" form="contact-form"/>
+        <div class="wrap-input">
+          <div class="wrap-input_text">
+            <input placeholder="Your name"/>
+          </div>
+          <div class="wrap-input_text">
+            <input placeholder="Your email"/>
+          </div>
+          <div class="wrap-input_text">
+            <textarea placeholder="Message" form="contact-form" type="text"/>
+          </div>
+        </div>
+        <div class="submit-btn">
+          <button type="submit" form="contact-form" value="Submit">Send</button>
+        </div>
       </form>
     </section>
   `;
